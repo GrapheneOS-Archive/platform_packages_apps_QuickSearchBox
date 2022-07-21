f6f571e41144756698e818997ce64b6b32e2014f
Ryan O'Leary <ryanaoleary@google.com>
2022-07-21 01:05:26 +0000

    Conversion of AbstractSource.java to AbstractSource.kt in AOSP QuickSearchBox App
    Used automated tool to convert AbstractSource.java to Kotlin code, fixing all errors and warnings arising from the conversion. Also configured Android.bp to build target QuickSearchBox with the new .kt files and added an exclude_srcs property for now unused java files. Built converted files against the latest version of Kotlin (-language-version 1.7)
    
    Bug: 239665416
    
    Test:  manual testing with bramble device, unit tests ran with atest QuickSearchBoxTests with result:
    Summary (Test executed with 1 devices.)
    -------
    arm64-v8a QuickSearchBoxTests: Passed: 50, Failed: 0, Ignored: 0, Assumption Failed: 0,
    
    All tests passed!
    
    Change-Id: I16b521d2bc83be24ad9f22fae3c4cdfad65562c8

--- src/com/android/quicksearchbox/AbstractSource.kt
+++ src/com/android/quicksearchbox/AbstractSource.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,121 +13,129 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox
 
-package com.android.quicksearchbox;
-
-import com.android.quicksearchbox.util.NamedTaskExecutor;
-import com.android.quicksearchbox.util.NowOrLater;
-
-import android.app.SearchManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
+import com.android.quicksearchbox.util.NamedTaskExecutor
+import com.android.quicksearchbox.util.NowOrLater
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 
 /**
  * Abstract suggestion source implementation.
  */
-public abstract class AbstractSource implements Source {
-
-    private static final String TAG = "QSB.AbstractSource";
-
-    private final Context mContext;
-    private final Handler mUiThread;
-
-    private IconLoader mIconLoader;
-
-    private final NamedTaskExecutor mIconLoaderExecutor;
-
-    public AbstractSource(Context context, Handler uiThread, NamedTaskExecutor iconLoader) {
-        mContext = context;
-        mUiThread = uiThread;
-        mIconLoaderExecutor = iconLoader;
-    }
-
-    protected Context getContext() {
-        return mContext;
-    }
-
-    protected IconLoader getIconLoader() {
-        if (mIconLoader == null) {
-            String iconPackage = getIconPackage();
-            mIconLoader = new CachingIconLoader(
-                    new PackageIconLoader(mContext, iconPackage, mUiThread, mIconLoaderExecutor));
+abstract class AbstractSource(context: Context, uiThread: Handler, iconLoader: NamedTaskExecutor) :
+    com.android.quicksearchbox.Source {
+    private val mContext: Context
+    private val mUiThread: Handler
+    private var mIconLoader: com.android.quicksearchbox.IconLoader? = null
+    private val mIconLoaderExecutor: NamedTaskExecutor
+    protected val context: Context
+        get() = mContext
+    protected val iconLoader: com.android.quicksearchbox.IconLoader?
+        get() {
+            if (mIconLoader == null) {
+                val iconPackage = iconPackage
+                mIconLoader = com.android.quicksearchbox.CachingIconLoader(
+                    com.android.quicksearchbox.PackageIconLoader(
+                        mContext,
+                        iconPackage,
+                        mUiThread,
+                        mIconLoaderExecutor
+                    )
+                )
+            }
+            return mIconLoader
         }
-        return mIconLoader;
-    }
-
-    protected abstract String getIconPackage();
-
+    protected abstract val iconPackage: String
     @Override
-    public NowOrLater<Drawable> getIcon(String drawableId) {
-        return getIconLoader().getIcon(drawableId);
+    override fun getIcon(drawableId: String): NowOrLater<Drawable>? {
+        return iconLoader?.getIcon(drawableId)
     }
 
     @Override
-    public Uri getIconUri(String drawableId) {
-        return getIconLoader().getIconUri(drawableId);
+    override fun getIconUri(drawableId: String): Uri? {
+        return iconLoader?.getIconUri(drawableId)
     }
 
     @Override
-    public Intent createSearchIntent(String query, Bundle appData) {
-        return createSourceSearchIntent(getIntentComponent(), query, appData);
+    override fun createSearchIntent(query: String, appData: Bundle): Intent? {
+        return AbstractSource.Companion.createSourceSearchIntent(
+            getIntentComponent(),
+            query,
+            appData
+        )
     }
 
-    public static Intent createSourceSearchIntent(ComponentName activity, String query,
-            Bundle appData) {
-        if (activity == null) {
-            Log.w(TAG, "Tried to create search intent with no target activity");
-            return null;
-        }
-        Intent intent = new Intent(Intent.ACTION_SEARCH);
-        intent.setComponent(activity);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        // We need CLEAR_TOP to avoid reusing an old task that has other activities
-        // on top of the one we want.
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.putExtra(SearchManager.USER_QUERY, query);
-        intent.putExtra(SearchManager.QUERY, query);
-        if (appData != null) {
-            intent.putExtra(SearchManager.APP_DATA, appData);
-        }
-        return intent;
+    protected fun createVoiceWebSearchIntent(appData: Bundle?): Intent {
+        return com.android.quicksearchbox.QsbApplication.get(mContext).getVoiceSearch()
+            .createVoiceWebSearchIntent(appData)
     }
 
-    protected Intent createVoiceWebSearchIntent(Bundle appData) {
-        return QsbApplication.get(mContext).getVoiceSearch()
-                .createVoiceWebSearchIntent(appData);
+    override fun getRoot() : Source {
+        return this
     }
 
     @Override
-    public Source getRoot() {
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o != null && o instanceof Source) {
-            Source s = ((Source) o).getRoot();
-            if (s.getClass().equals(this.getClass())) {
-                return s.getName().equals(getName());
+    override fun equals(other: Any?): Boolean {
+        if (other is com.android.quicksearchbox.Source) {
+            val s: com.android.quicksearchbox.Source =
+                other.getRoot()
+            if (s::class.equals(this::class)) {
+                return s.getName().equals(getName())
             }
         }
-        return false;
+        return false
     }
 
     @Override
-    public int hashCode() {
-        return getName().hashCode();
+    override fun hashCode(): Int {
+        return getName().hashCode()
     }
 
     @Override
-    public String toString() {
-        return "Source{name=" + getName() + "}";
+    override fun toString(): String {
+        return "Source{name=" + getName().toString() + "}"
     }
 
+    companion object {
+        private const val TAG = "QSB.AbstractSource"
+        @JvmStatic
+        fun createSourceSearchIntent(
+            activity: ComponentName?, query: String?,
+            appData: Bundle?
+        ): Intent? {
+            if (activity == null) {
+                Log.w(
+                    AbstractSource.Companion.TAG,
+                    "Tried to create search intent with no target activity"
+                )
+                return null
+            }
+            val intent = Intent(Intent.ACTION_SEARCH)
+            intent.setComponent(activity)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            // We need CLEAR_TOP to avoid reusing an old task that has other activities
+            // on top of the one we want.
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            intent.putExtra(SearchManager.USER_QUERY, query)
+            intent.putExtra(SearchManager.QUERY, query)
+            if (appData != null) {
+                intent.putExtra(SearchManager.APP_DATA, appData)
+            }
+            return intent
+        }
+    }
+
+    init {
+        mContext = context
+        mUiThread = uiThread
+        mIconLoaderExecutor = iconLoader
+    }
 }
