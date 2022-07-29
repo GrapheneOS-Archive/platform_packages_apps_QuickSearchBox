095ad5d1ae716de5dc631d87a24d3127824a21bd
Ryan O'Leary <ryanaoleary@google.com>
2022-07-29 01:47:42 +0000

    Initial conversion of SuggestionCursorBackedCursor.java to SuggestionCursorBackedCursor.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: Ic88d68903437bdb93440fdddbdb21938f01cf725

--- src/com/android/quicksearchbox/SuggestionCursorBackedCursor.kt
+++ src/com/android/quicksearchbox/SuggestionCursorBackedCursor.kt
@@ -13,188 +13,171 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.quicksearchbox;
+package com.android.quicksearchbox
 
-import android.app.SearchManager;
-import android.database.AbstractCursor;
-import android.database.CursorIndexOutOfBoundsException;
+import android.app.SearchManager
+import android.database.AbstractCursor
+import android.database.CursorIndexOutOfBoundsException
+import java.util.ArrayList
+import java.util.Arrays
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
-public class SuggestionCursorBackedCursor extends AbstractCursor {
-
-    // This array also used in CursorBackedSuggestionExtras to avoid duplication.
-    public static final String[] COLUMNS = {
-        "_id",  // 0, This will contain the row number. CursorAdapter, used by SuggestionsAdapter,
-                // used by SearchDialog, expects an _id column.
-        SearchManager.SUGGEST_COLUMN_TEXT_1,  // 1
-        SearchManager.SUGGEST_COLUMN_TEXT_2,  // 2
-        SearchManager.SUGGEST_COLUMN_TEXT_2_URL,  // 3
-        SearchManager.SUGGEST_COLUMN_ICON_1,  // 4
-        SearchManager.SUGGEST_COLUMN_ICON_2,  // 5
-        SearchManager.SUGGEST_COLUMN_INTENT_ACTION,  // 6
-        SearchManager.SUGGEST_COLUMN_INTENT_DATA,  // 7
-        SearchManager.SUGGEST_COLUMN_INTENT_EXTRA_DATA,  // 8
-        SearchManager.SUGGEST_COLUMN_QUERY,  // 9
-        SearchManager.SUGGEST_COLUMN_FORMAT,  // 10
-        SearchManager.SUGGEST_COLUMN_SHORTCUT_ID,  // 11
-        SearchManager.SUGGEST_COLUMN_SPINNER_WHILE_REFRESHING,  // 12
-    };
-
-    private static final int COLUMN_INDEX_ID = 0;
-    private static final int COLUMN_INDEX_TEXT1 = 1;
-    private static final int COLUMN_INDEX_TEXT2 = 2;
-    private static final int COLUMN_INDEX_TEXT2_URL = 3;
-    private static final int COLUMN_INDEX_ICON1 = 4;
-    private static final int COLUMN_INDEX_ICON2 = 5;
-    private static final int COLUMN_INDEX_INTENT_ACTION = 6;
-    private static final int COLUMN_INDEX_INTENT_DATA = 7;
-    private static final int COLUMN_INDEX_INTENT_EXTRA_DATA = 8;
-    private static final int COLUMN_INDEX_QUERY = 9;
-    private static final int COLUMN_INDEX_FORMAT = 10;
-    private static final int COLUMN_INDEX_SHORTCUT_ID = 11;
-    private static final int COLUMN_INDEX_SPINNER_WHILE_REFRESHING = 12;
-
-    private final SuggestionCursor mCursor;
-    private ArrayList<String> mExtraColumns;
-
-    public SuggestionCursorBackedCursor(SuggestionCursor cursor) {
-        mCursor = cursor;
+class SuggestionCursorBackedCursor(private val mCursor: SuggestionCursor) :
+    AbstractCursor() {
+    private var mExtraColumns: ArrayList<String>? = null
+    @Override
+    fun close() {
+        super.close()
+        mCursor.close()
     }
 
-    @Override
-    public void close() {
-        super.close();
-        mCursor.close();
+    @get:Override
+    val columnNames: Array<String>
+        get() {
+            val extraColumns: Collection<String> = mCursor.getExtraColumns()
+            return if (extraColumns != null) {
+                val allColumns: ArrayList<String> = ArrayList<String>(
+                    COLUMNS.size +
+                            extraColumns.size()
+                )
+                mExtraColumns = ArrayList<String>(extraColumns)
+                allColumns.addAll(Arrays.asList(COLUMNS))
+                allColumns.addAll(mExtraColumns)
+                allColumns.toArray(arrayOfNulls<String>(allColumns.size()))
+            } else {
+                COLUMNS
+            }
+        }
+
+    @get:Override
+    val count: Int
+        get() = mCursor.getCount()
+
+    private fun get(): Suggestion {
+        mCursor.moveTo(getPosition())
+        return mCursor
     }
 
-    @Override
-    public String[] getColumnNames() {
-        Collection<String> extraColumns = mCursor.getExtraColumns();
-        if (extraColumns != null) {
-            ArrayList<String> allColumns = new ArrayList<String>(COLUMNS.length +
-                    extraColumns.size());
-            mExtraColumns = new ArrayList<String>(extraColumns);
-            allColumns.addAll(Arrays.asList(COLUMNS));
-            allColumns.addAll(mExtraColumns);
-            return allColumns.toArray(new String[allColumns.size()]);
+    private fun getExtra(columnIdx: Int): String? {
+        val extraColumn = columnIdx - COLUMNS.size
+        val extras: SuggestionExtras = get().getExtras()
+        return if (extras != null) {
+            extras.getExtra(mExtraColumns.get(extraColumn))
         } else {
-            return COLUMNS;
+            null
         }
     }
 
     @Override
-    public int getCount() {
-        return mCursor.getCount();
-    }
-
-    private Suggestion get() {
-        mCursor.moveTo(getPosition());
-        return mCursor;
-    }
-
-    private String getExtra(int columnIdx) {
-        int extraColumn = columnIdx - COLUMNS.length;
-        SuggestionExtras extras = get().getExtras();
-        if (extras != null) {
-            return extras.getExtra(mExtraColumns.get(extraColumn));
-        } else {
-            return null;
-        }
-    }
-
-    @Override
-    public int getInt(int column) {
-        if (column == COLUMN_INDEX_ID) {
-            return getPosition();
+    fun getInt(column: Int): Int {
+        return if (column == COLUMN_INDEX_ID) {
+            getPosition()
         } else {
             try {
-                return Integer.valueOf(getString(column));
-            } catch (NumberFormatException e) {
-                return 0;
+                Integer.valueOf(getString(column))
+            } catch (e: NumberFormatException) {
+                0
             }
         }
     }
 
     @Override
-    public String getString(int column) {
-        if (column < COLUMNS.length) {
-            switch (column) {
-                case COLUMN_INDEX_ID:
-                    return String.valueOf(getPosition());
-                case COLUMN_INDEX_TEXT1:
-                    return get().getSuggestionText1();
-                case COLUMN_INDEX_TEXT2:
-                    return get().getSuggestionText2();
-                case COLUMN_INDEX_TEXT2_URL:
-                    return get().getSuggestionText2Url();
-                case COLUMN_INDEX_ICON1:
-                    return get().getSuggestionIcon1();
-                case COLUMN_INDEX_ICON2:
-                    return get().getSuggestionIcon2();
-                case COLUMN_INDEX_INTENT_ACTION:
-                    return get().getSuggestionIntentAction();
-                case COLUMN_INDEX_INTENT_DATA:
-                    return get().getSuggestionIntentDataString();
-                case COLUMN_INDEX_INTENT_EXTRA_DATA:
-                    return get().getSuggestionIntentExtraData();
-                case COLUMN_INDEX_QUERY:
-                    return get().getSuggestionQuery();
-                case COLUMN_INDEX_FORMAT:
-                    return get().getSuggestionFormat();
-                case COLUMN_INDEX_SHORTCUT_ID:
-                    return get().getShortcutId();
-                case COLUMN_INDEX_SPINNER_WHILE_REFRESHING:
-                    return String.valueOf(get().isSpinnerWhileRefreshing());
-                default:
-                    throw new CursorIndexOutOfBoundsException("Requested column " + column
-                            + " of " + COLUMNS.length);
+    fun getString(column: Int): String? {
+        return if (column < COLUMNS.size) {
+            when (column) {
+                COLUMN_INDEX_ID -> String.valueOf(getPosition())
+                COLUMN_INDEX_TEXT1 -> get().getSuggestionText1()
+                COLUMN_INDEX_TEXT2 -> get().getSuggestionText2()
+                COLUMN_INDEX_TEXT2_URL -> get().getSuggestionText2Url()
+                COLUMN_INDEX_ICON1 -> get().getSuggestionIcon1()
+                COLUMN_INDEX_ICON2 -> get().getSuggestionIcon2()
+                COLUMN_INDEX_INTENT_ACTION -> get().getSuggestionIntentAction()
+                COLUMN_INDEX_INTENT_DATA -> get().getSuggestionIntentDataString()
+                COLUMN_INDEX_INTENT_EXTRA_DATA -> get().getSuggestionIntentExtraData()
+                COLUMN_INDEX_QUERY -> get().getSuggestionQuery()
+                COLUMN_INDEX_FORMAT -> get().getSuggestionFormat()
+                COLUMN_INDEX_SHORTCUT_ID -> get().getShortcutId()
+                COLUMN_INDEX_SPINNER_WHILE_REFRESHING -> String.valueOf(get().isSpinnerWhileRefreshing())
+                else -> throw CursorIndexOutOfBoundsException(
+                    "Requested column " + column
+                            + " of " + COLUMNS.size
+                )
             }
         } else {
-            return getExtra(column);
+            getExtra(column)
         }
     }
 
     @Override
-    public long getLong(int column) {
-        try {
-            return Long.valueOf(getString(column));
-        } catch (NumberFormatException e) {
-            return 0;
+    fun getLong(column: Int): Long {
+        return try {
+            Long.valueOf(getString(column))
+        } catch (e: NumberFormatException) {
+            0
         }
     }
 
     @Override
-    public boolean isNull(int column) {
-        return getString(column) == null;
+    fun isNull(column: Int): Boolean {
+        return getString(column) == null
     }
 
     @Override
-    public short getShort(int column) {
-        try {
-            return Short.valueOf(getString(column));
-        } catch (NumberFormatException e) {
-            return 0;
+    fun getShort(column: Int): Short {
+        return try {
+            Short.valueOf(getString(column))
+        } catch (e: NumberFormatException) {
+            0
         }
     }
 
     @Override
-    public double getDouble(int column) {
-        try {
-            return Double.valueOf(getString(column));
-        } catch (NumberFormatException e) {
-            return 0;
+    fun getDouble(column: Int): Double {
+        return try {
+            Double.valueOf(getString(column))
+        } catch (e: NumberFormatException) {
+            0
         }
     }
 
     @Override
-    public float getFloat(int column) {
-        try {
-            return Float.valueOf(getString(column));
-        } catch (NumberFormatException e) {
-            return 0;
+    fun getFloat(column: Int): Float {
+        return try {
+            Float.valueOf(getString(column))
+        } catch (e: NumberFormatException) {
+            0
         }
+    }
+
+    companion object {
+        // This array also used in CursorBackedSuggestionExtras to avoid duplication.
+        val COLUMNS = arrayOf(
+            "_id",  // 0, This will contain the row number. CursorAdapter, used by SuggestionsAdapter,
+            // used by SearchDialog, expects an _id column.
+            SearchManager.SUGGEST_COLUMN_TEXT_1,  // 1
+            SearchManager.SUGGEST_COLUMN_TEXT_2,  // 2
+            SearchManager.SUGGEST_COLUMN_TEXT_2_URL,  // 3
+            SearchManager.SUGGEST_COLUMN_ICON_1,  // 4
+            SearchManager.SUGGEST_COLUMN_ICON_2,  // 5
+            SearchManager.SUGGEST_COLUMN_INTENT_ACTION,  // 6
+            SearchManager.SUGGEST_COLUMN_INTENT_DATA,  // 7
+            SearchManager.SUGGEST_COLUMN_INTENT_EXTRA_DATA,  // 8
+            SearchManager.SUGGEST_COLUMN_QUERY,  // 9
+            SearchManager.SUGGEST_COLUMN_FORMAT,  // 10
+            SearchManager.SUGGEST_COLUMN_SHORTCUT_ID,  // 11
+            SearchManager.SUGGEST_COLUMN_SPINNER_WHILE_REFRESHING
+        )
+        private const val COLUMN_INDEX_ID = 0
+        private const val COLUMN_INDEX_TEXT1 = 1
+        private const val COLUMN_INDEX_TEXT2 = 2
+        private const val COLUMN_INDEX_TEXT2_URL = 3
+        private const val COLUMN_INDEX_ICON1 = 4
+        private const val COLUMN_INDEX_ICON2 = 5
+        private const val COLUMN_INDEX_INTENT_ACTION = 6
+        private const val COLUMN_INDEX_INTENT_DATA = 7
+        private const val COLUMN_INDEX_INTENT_EXTRA_DATA = 8
+        private const val COLUMN_INDEX_QUERY = 9
+        private const val COLUMN_INDEX_FORMAT = 10
+        private const val COLUMN_INDEX_SHORTCUT_ID = 11
+        private const val COLUMN_INDEX_SPINNER_WHILE_REFRESHING = 12
     }
 }
