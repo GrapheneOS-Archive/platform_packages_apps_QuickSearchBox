2f611c4ad4bf32ed150cbf2bfb4ca955b5bb488e
Ryan O'Leary <ryanaoleary@google.com>
2022-08-03 02:01:21 +0000

    Initial conversion of CursorBackedSuggestionExtras.java to CursorBackedSuggestionExtras.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I189034a5395d74b5dd2c7163ca59355e6d074743

--- src/com/android/quicksearchbox/IconLoader.kt
+++ src/com/android/quicksearchbox/IconLoader.kt
@@ -13,21 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox
 
-package com.android.quicksearchbox;
-
-import com.android.quicksearchbox.util.NowOrLater;
-
-import android.content.ContentResolver;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.content.ContentResolver
 
 /**
  * Interface for icon loaders.
  *
  */
-public interface IconLoader {
-
+interface IconLoader {
     /**
      * Gets a drawable given an ID.
      *
@@ -35,22 +29,21 @@ public interface IconLoader {
      * (e.g., "2130837524"), in which case we will try to retrieve a drawable from
      * the provider's resources. If the ID is not an integer, it is
      * treated as a Uri and opened with
-     * {@link ContentResolver#openOutputStream(android.net.Uri, String)}.
+     * [ContentResolver.openOutputStream].
      *
      * All resources and URIs are read using the suggestion provider's context.
      *
-     * @return a {@link NowOrLater} for retrieving the icon. If the ID is not formatted as expected,
-     *      or no drawable can be found for the provided value, the value from this will be null.
+     * @return a [NowOrLater] for retrieving the icon. If the ID is not formatted as expected,
+     * or no drawable can be found for the provided value, the value from this will be null.
      *
      * @param drawableId a string like "2130837524",
-     *        "android.resource://com.android.alarmclock/2130837524",
-     *        or "content://contacts/photos/253".
+     * "android.resource://com.android.alarmclock/2130837524",
+     * or "content://contacts/photos/253".
      */
-    NowOrLater<Drawable> getIcon(String drawableId);
+    fun getIcon(drawableId: String?): NowOrLater<Drawable?>?
 
     /**
      * Converts a drawable ID to a Uri that can be used from other packages.
      */
-    Uri getIconUri(String drawableId);
-
+    fun getIconUri(drawableId: String?): Uri?
 }
