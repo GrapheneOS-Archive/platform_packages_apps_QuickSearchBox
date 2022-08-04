9c36d1ec187967dcea5e07e279a5ba1ae8ebd5d7
Ryan O'Leary <ryanaoleary@google.com>
2022-08-04 21:25:24 +0000

    Initial conversion of CursorBackedSourceResult.java to CursorBackedSourceResult.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I6bd30aebf0e264a8f7fe0ac31dc674c099030eee

--- src/com/android/quicksearchbox/CursorBackedSourceResult.kt
+++ src/com/android/quicksearchbox/CursorBackedSourceResult.kt
@@ -13,65 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.quicksearchbox;
+package com.android.quicksearchbox
 
-import android.content.ComponentName;
-import android.database.Cursor;
+import android.content.ComponentName
+import com.android.quicksearchbox.google.GoogleSource
 
-import com.android.quicksearchbox.google.GoogleSource;
+class CursorBackedSourceResult(
+    @get:Override override val suggestionSource: GoogleSource,
+    userQuery: String?,
+    cursor: Cursor?
+) : CursorBackedSuggestionCursor(userQuery, cursor), SourceResult {
+    constructor(source: GoogleSource, userQuery: String?) : this(source, userQuery, null)
 
-import java.util.Collection;
-
-public class CursorBackedSourceResult extends CursorBackedSuggestionCursor
-        implements SourceResult {
-
-    private final GoogleSource mSource;
-
-    public CursorBackedSourceResult(GoogleSource source, String userQuery) {
-        this(source, userQuery, null);
-    }
-
-    public CursorBackedSourceResult(GoogleSource source, String userQuery, Cursor cursor) {
-        super(userQuery, cursor);
-        mSource = source;
-    }
-
-    public GoogleSource getSource() {
-        return mSource;
-    }
+    override val suggestionIntentComponent: ComponentName
+        @Override get() = suggestionSource.getIntentComponent()
+    override val isSuggestionShortcut: Boolean
+        get() = false
+    override val isHistorySuggestion: Boolean
+        get() = false
 
     @Override
-    public GoogleSource getSuggestionSource() {
-        return mSource;
+    override fun toString(): String {
+        return suggestionSource.toString() + "[" + getUserQuery() + "]"
     }
 
-    @Override
-    public ComponentName getSuggestionIntentComponent() {
-        return mSource.getIntentComponent();
-    }
-
-    public boolean isSuggestionShortcut() {
-        return false;
-    }
-
-    public boolean isHistorySuggestion() {
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return mSource + "[" + getUserQuery() + "]";
-    }
-
-    @Override
-    public SuggestionExtras getExtras() {
-        if (mCursor == null) return null;
-        return CursorBackedSuggestionExtras.createExtrasIfNecessary(mCursor, getPosition());
-    }
-
-    public Collection<String> getExtraColumns() {
-        if (mCursor == null) return null;
-        return CursorBackedSuggestionExtras.getExtraColumns(mCursor);
-    }
-
+    override val extras: SuggestionExtras
+        @Override get() = if (mCursor == null) null else CursorBackedSuggestionExtras.createExtrasIfNecessary(
+            mCursor,
+            getPosition()
+        )!!
+    override val extraColumns: Collection<String>?
+        get() = if (mCursor == null) null else CursorBackedSuggestionExtras.getExtraColumns(
+            mCursor
+        )
 }
