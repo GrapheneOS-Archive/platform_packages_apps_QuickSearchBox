bee23cb9a130f28b26b810394af6eaf35cae680d
Ryan O'Leary <ryanaoleary@google.com>
2022-08-04 19:02:48 +0000

    Initial conversion of NowOrLater.java to NowOrLater.kt in AOSP QuickSearchBox App using automatic conversion tool.
    No additional changes were needed for NowOrLater.kt.
    
    Bug: 239665416
    
    Test:  built against Kotlin 1.7 and with -Werror turned on, tested manually on bramble device and ran atest QuickSearchBoxTests with result:
    Summary (Test executed with 1 devices.)
    -------
    arm64-v8a QuickSearchBoxTests: Passed: 50, Failed: 0, Ignored: 0, Assumption Failed: 0,
    
    All tests passed!
    
    Change-Id: I196fa22bd36a99ad327d23eb47f7cc8ff05bfc2c

--- src/com/android/quicksearchbox/util/NowOrLater.kt
+++ src/com/android/quicksearchbox/util/NowOrLater.kt
@@ -13,31 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.quicksearchbox.util;
+package com.android.quicksearchbox.util
 
 /**
  * Interface for an object that may be constructed asynchronously. In cases when the object is ready
  * (on constructible) immediately, it provides synchronous access to it. Otherwise, the object can
- * be sent to a {@link Consumer} later.
+ * be sent to a [Consumer] later.
  */
-public interface NowOrLater<C> {
-
+interface NowOrLater<C> {
     /**
      * Indicates if the object is ready (or constructible synchronously).
      */
-    boolean haveNow();
+    fun haveNow(): Boolean
 
     /**
-     * Gets the object now. Should only be called if {@link #haveNow()} returns {@code true},
-     * otherwise an {@link IllegalStateException} will be thrown.
+     * Gets the object now. Should only be called if [.haveNow] returns `true`,
+     * otherwise an [IllegalStateException] will be thrown.
      */
-    C getNow();
+    val now: C
 
     /**
      * Request the object asynchronously. This can be called even if the object is ready now, in
      * which case the callback may be made in context. The thread on which the consumer is called
      * back depends on the implementation.
      */
-    void getLater(Consumer<? super C> consumer);
-
+    fun getLater(consumer: Consumer<in C>?)
 }
