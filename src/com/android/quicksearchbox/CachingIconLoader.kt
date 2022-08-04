66ede2b5ef7cbe9571504612671386d2be42e1cc
Ryan O'Leary <ryanaoleary@google.com>
2022-08-04 20:40:49 +0000

    Initial conversion of CachingIconLoader.java to CachingIconLoader.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: Id293d8ea49bd125c288ed66e31aa2e165337d554

--- src/com/android/quicksearchbox/CachingIconLoader.kt
+++ src/com/android/quicksearchbox/CachingIconLoader.kt
@@ -13,128 +13,125 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox
 
-package com.android.quicksearchbox;
-
-import com.android.quicksearchbox.util.CachedLater;
-import com.android.quicksearchbox.util.Consumer;
-import com.android.quicksearchbox.util.Now;
-import com.android.quicksearchbox.util.NowOrLater;
-import com.android.quicksearchbox.util.NowOrLaterWrapper;
-
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.text.TextUtils;
-import android.util.Log;
-
-import java.util.WeakHashMap;
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.text.TextUtils
+import android.util.Log
+import com.android.quicksearchbox.util.*
+import java.util.WeakHashMap
 
 /**
  * Icon loader that caches the results of another icon loader.
  *
  */
-public class CachingIconLoader implements IconLoader {
+class CachingIconLoader(private val mWrapped: IconLoader) : IconLoader {
+    private val mIconCache: WeakHashMap<String, Entry>
+    override fun getIcon(drawableId: String): NowOrLater<Drawable> {
+        if (DBG) Log.d(
+            TAG,
+            "getIcon($drawableId)"
+        )
+        if (TextUtils.isEmpty(drawableId) || "0".equals(drawableId)) {
+            return Now(null)
+        }
+        var newEntry: Entry? = null
+        var drawableState: NowOrLater<Drawable.ConstantState?>?
+        synchronized(this) {
+            drawableState = queryCache(drawableId)
+            if (drawableState == null) {
+                newEntry = Entry()
+                storeInIconCache(drawableId, newEntry)
+            }
+        }
+        if (drawableState != null) {
+            return object : NowOrLaterWrapper<Drawable.ConstantState?, Drawable?>(drawableState) {
+                @Override
+                override fun get(value: Drawable.ConstantState): Drawable {
+                    return if (value == null) null else value.newDrawable()
+                }
+            }
+        }
+        val drawable: NowOrLater<Drawable?>? = mWrapped.getIcon(drawableId)
+        newEntry!!.set(drawable)
+        storeInIconCache(drawableId, newEntry)
+        return drawable!!
+    }
 
-    private static final boolean DBG = false;
-    private static final String TAG = "QSB.CachingIconLoader";
+    override fun getIconUri(drawableId: String): Uri {
+        return mWrapped.getIconUri(drawableId)
+    }
 
-    private final IconLoader mWrapped;
+    @Synchronized
+    private fun queryCache(drawableId: String): NowOrLater<Drawable.ConstantState?>? {
+        val cached: NowOrLater<Drawable.ConstantState?> = mIconCache.get(drawableId)
+        if (DBG) {
+            if (cached != null) Log.d(
+                TAG,
+                "Found icon in cache: $drawableId"
+            )
+        }
+        return cached
+    }
 
-    private final WeakHashMap<String, Entry> mIconCache;
+    @Synchronized
+    private fun storeInIconCache(resourceUri: String, drawable: Entry?) {
+        if (drawable != null) {
+            mIconCache.put(resourceUri, drawable)
+        }
+    }
+
+    private class Entry : CachedLater<Drawable.ConstantState?>(),
+        Consumer<Drawable?> {
+        private var mDrawable: NowOrLater<Drawable?>? = null
+        private var mGotDrawable = false
+        private var mCreateRequested = false
+        @Synchronized
+        fun set(drawable: NowOrLater<Drawable?>?) {
+            if (mGotDrawable) throw IllegalStateException("set() may only be called once.")
+            mGotDrawable = true
+            mDrawable = drawable
+            if (mCreateRequested) {
+                later
+            }
+        }
+
+        @Override
+        @Synchronized
+        override fun create() {
+            if (!mCreateRequested) {
+                mCreateRequested = true
+                if (mGotDrawable) {
+                    later
+                }
+            }
+        }
+
+        private val later: Unit
+            private get() {
+                val drawable: NowOrLater<Drawable?>? = mDrawable
+                mDrawable = null
+                drawable!!.getLater(this)
+            }
+
+        override fun consume(value: Drawable): Boolean {
+            store(if (value == null) null else value.getConstantState())
+            return true
+        }
+    }
+
+    companion object {
+        private const val DBG = false
+        private const val TAG = "QSB.CachingIconLoader"
+    }
 
     /**
      * Creates a new caching icon loader.
      *
      * @param wrapped IconLoader whose results will be cached.
      */
-    public CachingIconLoader(IconLoader wrapped) {
-        mWrapped = wrapped;
-        mIconCache = new WeakHashMap<String, Entry>();
+    init {
+        mIconCache = WeakHashMap<String, Entry>()
     }
-
-    public NowOrLater<Drawable> getIcon(String drawableId) {
-        if (DBG) Log.d(TAG, "getIcon(" + drawableId + ")");
-        if (TextUtils.isEmpty(drawableId) || "0".equals(drawableId)) {
-            return new Now<Drawable>(null);
-        }
-        Entry newEntry = null;
-        NowOrLater<Drawable.ConstantState> drawableState;
-        synchronized (this) {
-            drawableState = queryCache(drawableId);
-            if (drawableState == null) {
-                newEntry = new Entry();
-                storeInIconCache(drawableId, newEntry);
-            }
-        }
-        if (drawableState != null) {
-            return new NowOrLaterWrapper<Drawable.ConstantState, Drawable>(drawableState){
-                @Override
-                public Drawable get(Drawable.ConstantState value) {
-                    return value == null ? null : value.newDrawable();
-                }};
-        }
-        NowOrLater<Drawable> drawable = mWrapped.getIcon(drawableId);
-        newEntry.set(drawable);
-        storeInIconCache(drawableId, newEntry);
-        return drawable;
-    }
-
-    public Uri getIconUri(String drawableId) {
-        return mWrapped.getIconUri(drawableId);
-    }
-
-    private synchronized NowOrLater<Drawable.ConstantState> queryCache(String drawableId) {
-        NowOrLater<Drawable.ConstantState> cached = mIconCache.get(drawableId);
-        if (DBG) {
-            if (cached != null) Log.d(TAG, "Found icon in cache: " + drawableId);
-        }
-        return cached;
-    }
-
-    private synchronized void storeInIconCache(String resourceUri, Entry drawable) {
-        if (drawable != null) {
-            mIconCache.put(resourceUri, drawable);
-        }
-    }
-
-    private static class Entry extends CachedLater<Drawable.ConstantState>
-            implements Consumer<Drawable>{
-        private NowOrLater<Drawable> mDrawable;
-        private boolean mGotDrawable;
-        private boolean mCreateRequested;
-
-        public Entry() {
-        }
-
-        public synchronized void set(NowOrLater<Drawable> drawable) {
-            if (mGotDrawable) throw new IllegalStateException("set() may only be called once.");
-            mGotDrawable = true;
-            mDrawable = drawable;
-            if (mCreateRequested) {
-                getLater();
-            }
-        }
-
-        @Override
-        protected synchronized void create() {
-            if (!mCreateRequested) {
-                mCreateRequested = true;
-                if (mGotDrawable) {
-                    getLater();
-                }
-            }
-        }
-
-        private void getLater() {
-            NowOrLater<Drawable> drawable = mDrawable;
-            mDrawable = null;
-            drawable.getLater(this);
-        }
-
-        public boolean consume(Drawable value) {
-            store(value == null ? null : value.getConstantState());
-            return true;
-        }
-    }
-
 }
