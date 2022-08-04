173ce8dd6c893db600767284f50d0629dcad2283
Ryan O'Leary <ryanaoleary@google.com>
2022-08-04 00:32:02 +0000

    Initial conversion of Factory.java to Factory.kt in AOSP QuickSearchBox App using automatic conversion tool.
    No additional changes were needed for Factory.kt.
    
    Bug: 239665416
    
    Test:  built against Kotlin 1.7 and with -Werror turned on, tested manually on bramble device and ran atest QuickSearchBoxTests with result:
    Summary (Test executed with 1 devices.)
    -------
    arm64-v8a QuickSearchBoxTests: Passed: 50, Failed: 0, Ignored: 0, Assumption Failed: 0,
    
    All tests passed!
    
    Change-Id: Idb448183b823bb7b933cd0e071003352e4e9f544

--- src/com/android/quicksearchbox/util/Factory.kt
+++ src/com/android/quicksearchbox/util/Factory.kt
@@ -13,11 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox.util
 
-package com.android.quicksearchbox.util;
-
-public interface Factory<A> {
-
-    A create();
-
+interface Factory<A> {
+    fun create(): A
 }
