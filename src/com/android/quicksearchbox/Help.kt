d7df732ce8fcfb3c0a5906321d01cb013b725307
Ryan O'Leary <ryanaoleary@google.com>
2022-08-08 19:19:49 +0000

    Initial conversion of Help.java to Help.kt in AOSP QuickSearchBox App using automatic conversion tool.
    No additional changes were needed for Help.kt.
    
    Bug: 239665416
    
    Test:  built against Kotlin 1.7 and with -Werror turned on, tested manually on bramble device and ran atest QuickSearchBoxTests with result:
    Summary (Test executed with 1 devices.)
    -------
    arm64-v8a QuickSearchBoxTests: Passed: 50, Failed: 0, Ignored: 0, Assumption Failed: 0,
    
    All tests passed!
    
    Change-Id: Ice8b976e54912aeccf6c9cb4fecdce837957e28a

--- src/com/android/quicksearchbox/Help.kt
+++ src/com/android/quicksearchbox/Help.kt
@@ -13,49 +13,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.quicksearchbox;
+package com.android.quicksearchbox
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 /**
  * Handles app help.
  */
-public class Help {
-
-    private final Context mContext;
-    private final Config mConfig;
-
-    public Help(Context context, Config config) {
-        mContext = context;
-        mConfig = config;
+class Help(context: Context, config: Config) {
+    private val mContext: Context
+    private val mConfig: Config
+    fun addHelpMenuItem(menu: Menu, activityName: String?) {
+        addHelpMenuItem(menu, activityName, false)
     }
 
-    public void addHelpMenuItem(Menu menu, String activityName) {
-        addHelpMenuItem(menu, activityName, false);
-    }
-
-    public void addHelpMenuItem(Menu menu, String activityName, boolean showAsAction) {
-        Intent helpIntent = getHelpIntent(activityName);
+    fun addHelpMenuItem(menu: Menu, activityName: String?, showAsAction: Boolean) {
+        val helpIntent: Intent? = getHelpIntent(activityName)
         if (helpIntent != null) {
-            MenuInflater inflater = new MenuInflater(mContext);
-            inflater.inflate(R.menu.help, menu);
-            MenuItem item = menu.findItem(R.id.menu_help);
-            item.setIntent(helpIntent);
+            val inflater = MenuInflater(mContext)
+            inflater.inflate(R.menu.help, menu)
+            val item: MenuItem = menu.findItem(R.id.menu_help)
+            item.setIntent(helpIntent)
             if (showAsAction) {
-                item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+                item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
             }
         }
     }
 
-    private Intent getHelpIntent(String activityName) {
-        Uri helpUrl = mConfig.getHelpUrl(activityName);
-        if (helpUrl == null) return null;
-        return new Intent(Intent.ACTION_VIEW, helpUrl);
+    private fun getHelpIntent(activityName: String?): Intent? {
+        val helpUrl: Uri = mConfig.getHelpUrl(activityName) ?: return null
+        return Intent(Intent.ACTION_VIEW, helpUrl)
     }
 
+    init {
+        mContext = context
+        mConfig = config
+    }
 }
