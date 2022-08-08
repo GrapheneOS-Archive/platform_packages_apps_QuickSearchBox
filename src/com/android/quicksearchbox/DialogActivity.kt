9d910be292e91d25a7a0c2739189ebb55b63e9a7
Ryan O'Leary <ryanaoleary@google.com>
2022-08-08 17:29:08 +0000

    Initial conversion of DialogActivity.java to DialogActivity.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I14aad6e9cfdcfb91ded1617e7a6957c70918ee1e

--- src/com/android/quicksearchbox/DialogActivity.kt
+++ src/com/android/quicksearchbox/DialogActivity.kt
@@ -13,57 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox
 
-package com.android.quicksearchbox;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.FrameLayout
+import android.widget.TextView
 
 /**
  * Activity that looks like a dialog window.
  */
-public abstract class DialogActivity extends Activity {
+abstract class DialogActivity : Activity() {
+    @JvmField
+    protected var mTitleView: TextView? = null
 
-    protected TextView mTitleView;
-    protected FrameLayout mContentFrame;
+    @JvmField
+    protected var mContentFrame: FrameLayout? = null
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.dialog_activity);
-        mTitleView = (TextView) findViewById(R.id.alertTitle);
-        mContentFrame = (FrameLayout) findViewById(R.id.content);
+    protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_activity)
+        mTitleView = findViewById(R.id.alertTitle) as TextView?
+        mContentFrame = findViewById(R.id.content) as FrameLayout?
     }
 
-    public void setHeading(int titleRes) {
-        mTitleView.setText(titleRes);
+    fun setHeading(titleRes: Int) {
+        mTitleView.setText(titleRes)
     }
 
-    public void setHeading(CharSequence title) {
-        mTitleView.setText(title);
+    fun setHeading(title: CharSequence?) {
+        mTitleView.setText(title)
     }
 
-    public void setDialogContent(int layoutRes) {
-        mContentFrame.removeAllViews();
-        getLayoutInflater().inflate(layoutRes, mContentFrame);
+    fun setDialogContent(content: View?) {
+        mContentFrame.removeAllViews()
+        mContentFrame.addView(content)
     }
 
-    public void setDialogContent(View content) {
-        mContentFrame.removeAllViews();
-        mContentFrame.addView(content);
-    }
-
-    public View getDialogContent() {
-        if (mContentFrame.getChildCount() > 0) {
-            return mContentFrame.getChildAt(0);
+    var dialogContent: View?
+        get() = if (mContentFrame.getChildCount() > 0) {
+            mContentFrame.getChildAt(0)
         } else {
-            return null;
+            null
         }
-    }
-
+        set(layoutRes) {
+            mContentFrame.removeAllViews()
+            getLayoutInflater().inflate(layoutRes, mContentFrame)
+        }
 }
