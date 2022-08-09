fd9a714c7bf925ffa6d9d7b81515a1b62f375b3b
Ryan O'Leary <ryanaoleary@google.com>
2022-08-09 21:22:23 +0000

    Initial conversion of SuggestionUtils.java to SuggestionUtils.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I28e519c814f9d091c8b7e7e941353fe9c50cb1c8

--- src/com/android/quicksearchbox/SuggestionUtils.kt
+++ src/com/android/quicksearchbox/SuggestionUtils.kt
@@ -13,82 +13,79 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox
 
-package com.android.quicksearchbox;
-
-import com.google.common.annotations.VisibleForTesting;
-
-import android.app.SearchManager;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
+import com.google.common.annotations.VisibleForTesting
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 
 /**
  * Some utilities for suggestions.
  */
-public class SuggestionUtils {
-
-    private SuggestionUtils() {
-    }
-
-    public static Intent getSuggestionIntent(SuggestionCursor suggestion, Bundle appSearchData) {
-        String action = suggestion.getSuggestionIntentAction();
-
-        String data = suggestion.getSuggestionIntentDataString();
-        String query = suggestion.getSuggestionQuery();
-        String userQuery = suggestion.getUserQuery();
-        String extraData = suggestion.getSuggestionIntentExtraData();
+object SuggestionUtils {
+    @JvmStatic
+    fun getSuggestionIntent(suggestion: SuggestionCursor, appSearchData: Bundle?): Intent {
+        val action: String = suggestion.getSuggestionIntentAction()
+        val data: String = suggestion.getSuggestionIntentDataString()
+        val query: String = suggestion.getSuggestionQuery()
+        val userQuery: String = suggestion.getUserQuery()
+        val extraData: String = suggestion.getSuggestionIntentExtraData()
 
         // Now build the Intent
-        Intent intent = new Intent(action);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        val intent = Intent(action)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         // We need CLEAR_TOP to avoid reusing an old task that has other activities
         // on top of the one we want.
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         if (data != null) {
-            intent.setData(Uri.parse(data));
+            intent.setData(Uri.parse(data))
         }
-        intent.putExtra(SearchManager.USER_QUERY, userQuery);
+        intent.putExtra(SearchManager.USER_QUERY, userQuery)
         if (query != null) {
-            intent.putExtra(SearchManager.QUERY, query);
+            intent.putExtra(SearchManager.QUERY, query)
         }
         if (extraData != null) {
-            intent.putExtra(SearchManager.EXTRA_DATA_KEY, extraData);
+            intent.putExtra(SearchManager.EXTRA_DATA_KEY, extraData)
         }
         if (appSearchData != null) {
-            intent.putExtra(SearchManager.APP_DATA, appSearchData);
+            intent.putExtra(SearchManager.APP_DATA, appSearchData)
         }
-
-        intent.setComponent(suggestion.getSuggestionIntentComponent());
-        return intent;
+        intent.setComponent(suggestion.getSuggestionIntentComponent())
+        return intent
     }
 
     /**
      * Gets a unique key that identifies a suggestion. This is used to avoid
      * duplicate suggestions.
      */
-    public static String getSuggestionKey(Suggestion suggestion) {
-        String action = makeKeyComponent(suggestion.getSuggestionIntentAction());
-        String data = makeKeyComponent(normalizeUrl(suggestion.getSuggestionIntentDataString()));
-        String query = makeKeyComponent(normalizeUrl(suggestion.getSuggestionQuery()));
+    @JvmStatic
+    fun getSuggestionKey(suggestion: Suggestion): String {
+        val action: String =
+            SuggestionUtils.makeKeyComponent(suggestion.getSuggestionIntentAction())
+        val data: String =
+            SuggestionUtils.makeKeyComponent(SuggestionUtils.normalizeUrl(suggestion.getSuggestionIntentDataString()))
+        val query: String =
+            SuggestionUtils.makeKeyComponent(SuggestionUtils.normalizeUrl(suggestion.getSuggestionQuery()))
         // calculating accurate size of string builder avoids an allocation vs starting with
         // the default size and having to expand.
-        int size = action.length() + 2 + data.length() + query.length();
-        return new StringBuilder(size)
-                .append(action)
-                .append('#')
-                .append(data)
-                .append('#')
-                .append(query)
-                .toString();
+        val size: Int = action.length() + 2 + data.length() + query.length()
+        return StringBuilder(size)
+            .append(action)
+            .append('#')
+            .append(data)
+            .append('#')
+            .append(query)
+            .toString()
     }
 
-    private static String makeKeyComponent(String str) {
-        return str == null ? "" : str;
+    private fun makeKeyComponent(str: String?): String {
+        return str ?: ""
     }
 
-    private static final String SCHEME_SEPARATOR = "://";
-    private static final String DEFAULT_SCHEME = "http";
+    private const val SCHEME_SEPARATOR = "://"
+    private const val DEFAULT_SCHEME = "http"
 
     /**
      * Simple url normalization that adds http:// if no scheme exists, and
@@ -96,27 +93,28 @@ public class SuggestionUtils {
      * www.google.com/ -> http://www.google.com.  Used to prevent obvious
      * duplication of nav suggestions, bookmarks and urls entered by the user.
      */
+    @JvmStatic
     @VisibleForTesting
-    static String normalizeUrl(String url) {
-        String normalized;
+    fun normalizeUrl(url: String?): String? {
+        val normalized: String
         if (url != null) {
-            int start;
-            int schemePos = url.indexOf(SCHEME_SEPARATOR);
+            val start: Int
+            val schemePos: Int = url.indexOf(SuggestionUtils.SCHEME_SEPARATOR)
             if (schemePos == -1) {
                 // no scheme - add the default
-                normalized = DEFAULT_SCHEME + SCHEME_SEPARATOR + url;
-                start = DEFAULT_SCHEME.length() + SCHEME_SEPARATOR.length();
+                normalized = SuggestionUtils.DEFAULT_SCHEME + SuggestionUtils.SCHEME_SEPARATOR + url
+                start =
+                    SuggestionUtils.DEFAULT_SCHEME.length() + SuggestionUtils.SCHEME_SEPARATOR.length()
             } else {
-                normalized = url;
-                start = schemePos + SCHEME_SEPARATOR.length();
+                normalized = url
+                start = schemePos + SuggestionUtils.SCHEME_SEPARATOR.length()
             }
-            int end = normalized.length();
-            if (normalized.indexOf('/', start) == end - 1) {
-                end--;
+            var end: Int = normalized.length()
+            if (normalized.indexOf('/', start) === end - 1) {
+                end--
             }
-            return normalized.substring(0, end);
+            return normalized.substring(0, end)
         }
-        return url;
+        return url
     }
-
 }
