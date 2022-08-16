ea9285b4c8686168b7199f7147aa4476b8799fe6
Ryan O'Leary <ryanaoleary@google.com>
2022-08-16 17:25:59 +0000

    Initial conversion of JavaNetHttpHelper.java to JavaNetHttpHelper.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I35847854f09cb61c215b22db09460ba3eeb3f28b

--- src/com/android/quicksearchbox/util/JavaNetHttpHelper.kt
+++ src/com/android/quicksearchbox/util/JavaNetHttpHelper.kt
@@ -14,174 +14,174 @@
  * limitations under the License.
  */
 
-package com.android.quicksearchbox.util;
+package com.android.quicksearchbox.util
 
-import android.os.Build;
-import android.util.Log;
+import android.os.Build
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+/** Simple HTTP client API. */
+class JavaNetHttpHelper(rewriter: HttpHelper.UrlRewriter, userAgent: String) : HttpHelper {
+  private var mConnectTimeout = 0
+  private var mReadTimeout = 0
+  private val mUserAgent: String
+  private val mRewriter: HttpHelper.UrlRewriter
 
-/**
- * Simple HTTP client API.
- */
-public class JavaNetHttpHelper implements HttpHelper {
-    private static final String TAG = "QSB.JavaNetHttpHelper";
-    private static final boolean DBG = false;
+  /**
+   * Executes a GET request and returns the response content.
+   *
+   * @param request Request.
+   * @return The response content. This is the empty string if the response contained no content.
+   * @throws IOException If an IO error occurs.
+   * @throws HttpException If the response has a status code other than 200.
+   */
+  @Throws(IOException::class, HttpHelper.HttpException::class)
+  override operator fun get(request: HttpHelper.GetRequest): String {
+    return get(request.getUrl(), request.getHeaders())
+  }
 
-    private static final int BUFFER_SIZE = 1024 * 4;
-    private static final String USER_AGENT_HEADER = "User-Agent";
-    private static final String DEFAULT_CHARSET = "UTF-8";
-
-    private int mConnectTimeout;
-    private int mReadTimeout;
-    private final String mUserAgent;
-    private final HttpHelper.UrlRewriter mRewriter;
-
-    /**
-     * Creates a new HTTP helper.
-     *
-     * @param rewriter URI rewriter
-     * @param userAgent User agent string, e.g. "MyApp/1.0".
-     */
-    public JavaNetHttpHelper(UrlRewriter rewriter, String userAgent) {
-        mUserAgent = userAgent + " (" + Build.DEVICE + " " + Build.ID + ")";
-        mRewriter = rewriter;
+  /**
+   * Executes a GET request and returns the response content.
+   *
+   * @param url Request URI.
+   * @param requestHeaders Request headers.
+   * @return The response content. This is the empty string if the response contained no content.
+   * @throws IOException If an IO error occurs.
+   * @throws HttpException If the response has a status code other than 200.
+   */
+  @Throws(IOException::class, HttpHelper.HttpException::class)
+  override operator fun get(url: String, requestHeaders: Map<String, String>): String {
+    var c: HttpURLConnection? = null
+    return try {
+      c = createConnection(url, requestHeaders)
+      c.setRequestMethod("GET")
+      c.connect()
+      getResponseFrom(c)
+    } finally {
+      if (c != null) {
+        c.disconnect()
+      }
     }
+  }
 
-    /**
-     * Executes a GET request and returns the response content.
-     *
-     * @param request Request.
-     * @return The response content. This is the empty string if the response
-     *         contained no content.
-     * @throws IOException If an IO error occurs.
-     * @throws HttpException If the response has a status code other than 200.
-     */
-    public String get(GetRequest request) throws IOException, HttpException {
-        return get(request.getUrl(), request.getHeaders());
+  @Override
+  @Throws(IOException::class, HttpHelper.HttpException::class)
+  override fun post(request: HttpHelper.PostRequest): String {
+    return post(request.getUrl(), request.getHeaders(), request.getContent())
+  }
+
+  @Throws(IOException::class, HttpHelper.HttpException::class)
+  override fun post(url: String, requestHeaders: Map<String, String>, content: String): String {
+    var requestHeaders: Map<String, String>? = requestHeaders
+    var c: HttpURLConnection? = null
+    return try {
+      if (requestHeaders == null) {
+        requestHeaders = HashMap<String, String>()
+      }
+      requestHeaders.put(
+        "Content-Length",
+        Integer.toString(if (content == null) 0 else content.length())
+      )
+      c = createConnection(url, requestHeaders)
+      c.setDoOutput(content != null)
+      c.setRequestMethod("POST")
+      c.connect()
+      if (content != null) {
+        val writer = OutputStreamWriter(c.getOutputStream())
+        writer.write(content)
+        writer.close()
+      }
+      getResponseFrom(c)
+    } finally {
+      if (c != null) {
+        c.disconnect()
+      }
     }
+  }
 
-    /**
-     * Executes a GET request and returns the response content.
-     *
-     * @param url Request URI.
-     * @param requestHeaders Request headers.
-     * @return The response content. This is the empty string if the response
-     *         contained no content.
-     * @throws IOException If an IO error occurs.
-     * @throws HttpException If the response has a status code other than 200.
-     */
-    public String get(String url, Map<String,String> requestHeaders)
-            throws IOException, HttpException {
-        HttpURLConnection c = null;
-        try {
-            c = createConnection(url, requestHeaders);
-            c.setRequestMethod("GET");
-            c.connect();
-            return getResponseFrom(c);
-        } finally {
-            if (c != null) {
-                c.disconnect();
-            }
-        }
+  @Throws(IOException::class, HttpHelper.HttpException::class)
+  private fun createConnection(url: String, headers: Map<String, String>?): HttpURLConnection {
+    val u = URL(mRewriter.rewrite(url))
+    if (JavaNetHttpHelper.Companion.DBG)
+      Log.d(JavaNetHttpHelper.Companion.TAG, "URL=$url rewritten='$u'")
+    val c: HttpURLConnection = u.openConnection() as HttpURLConnection
+    if (headers != null) {
+      for (e in headers.entrySet()) {
+        val name: String = e.getKey()
+        val value: String = e.getValue()
+        if (JavaNetHttpHelper.Companion.DBG)
+          Log.d(JavaNetHttpHelper.Companion.TAG, "  $name: $value")
+        c.addRequestProperty(name, value)
+      }
     }
+    c.addRequestProperty(JavaNetHttpHelper.Companion.USER_AGENT_HEADER, mUserAgent)
+    if (mConnectTimeout != 0) {
+      c.setConnectTimeout(mConnectTimeout)
+    }
+    if (mReadTimeout != 0) {
+      c.setReadTimeout(mReadTimeout)
+    }
+    return c
+  }
 
+  @Throws(IOException::class, HttpHelper.HttpException::class)
+  private fun getResponseFrom(c: HttpURLConnection?): String {
+    if (c.getResponseCode() !== HttpURLConnection.HTTP_OK) {
+      throw HttpHelper.HttpException(c.getResponseCode(), c.getResponseMessage())
+    }
+    if (JavaNetHttpHelper.Companion.DBG) {
+      Log.d(
+        JavaNetHttpHelper.Companion.TAG,
+        "Content-Type: " +
+          c.getContentType().toString() +
+          " (assuming " +
+          JavaNetHttpHelper.Companion.DEFAULT_CHARSET.toString() +
+          ")"
+      )
+    }
+    val reader =
+      BufferedReader(
+        InputStreamReader(c.getInputStream(), JavaNetHttpHelper.Companion.DEFAULT_CHARSET)
+      )
+    val string: StringBuilder = StringBuilder()
+    val chars = CharArray(JavaNetHttpHelper.Companion.BUFFER_SIZE)
+    var bytes: Int
+    while (reader.read(chars).also { bytes = it } != -1) {
+      string.append(chars, 0, bytes)
+    }
+    return string.toString()
+  }
+
+  override fun setConnectTimeout(timeoutMillis: Int) {
+    mConnectTimeout = timeoutMillis
+  }
+
+  override fun setReadTimeout(timeoutMillis: Int) {
+    mReadTimeout = timeoutMillis
+  }
+
+  /** A Url rewriter that does nothing, i.e., returns the url that is passed to it. */
+  class PassThroughRewriter : HttpHelper.UrlRewriter {
     @Override
-    public String post(PostRequest request) throws IOException, HttpException {
-        return post(request.getUrl(), request.getHeaders(), request.getContent());
+    override fun rewrite(url: String): String {
+      return url
     }
+  }
 
-    public String post(String url, Map<String,String> requestHeaders, String content)
-            throws IOException, HttpException {
-        HttpURLConnection c = null;
-        try {
-            if (requestHeaders == null) {
-                requestHeaders = new HashMap<String, String>();
-            }
-            requestHeaders.put("Content-Length",
-                    Integer.toString(content == null ? 0 : content.length()));
-            c = createConnection(url, requestHeaders);
-            c.setDoOutput(content != null);
-            c.setRequestMethod("POST");
-            c.connect();
-            if (content != null) {
-                OutputStreamWriter writer = new OutputStreamWriter(c.getOutputStream());
-                writer.write(content);
-                writer.close();
-            }
-            return getResponseFrom(c);
-        } finally {
-            if (c != null) {
-                c.disconnect();
-            }
-        }
-    }
+  companion object {
+    private const val TAG = "QSB.JavaNetHttpHelper"
+    private const val DBG = false
+    private const val BUFFER_SIZE = 1024 * 4
+    private const val USER_AGENT_HEADER = "User-Agent"
+    private const val DEFAULT_CHARSET = "UTF-8"
+  }
 
-    private HttpURLConnection createConnection(String url, Map<String, String> headers)
-            throws IOException, HttpException {
-        URL u = new URL(mRewriter.rewrite(url));
-        if (DBG) Log.d(TAG, "URL=" + url + " rewritten='" + u + "'");
-        HttpURLConnection c = (HttpURLConnection) u.openConnection();
-        if (headers != null) {
-            for (Map.Entry<String,String> e : headers.entrySet()) {
-                String name = e.getKey();
-                String value = e.getValue();
-                if (DBG) Log.d(TAG, "  " + name + ": " + value);
-                c.addRequestProperty(name, value);
-            }
-        }
-        c.addRequestProperty(USER_AGENT_HEADER, mUserAgent);
-        if (mConnectTimeout != 0) {
-            c.setConnectTimeout(mConnectTimeout);
-        }
-        if (mReadTimeout != 0) {
-            c.setReadTimeout(mReadTimeout);
-        }
-        return c;
-    }
-
-    private String getResponseFrom(HttpURLConnection c) throws IOException, HttpException {
-        if (c.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            throw new HttpException(c.getResponseCode(), c.getResponseMessage());
-        }
-        if (DBG) {
-            Log.d(TAG, "Content-Type: " + c.getContentType() + " (assuming " +
-                    DEFAULT_CHARSET + ")");
-        }
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(c.getInputStream(), DEFAULT_CHARSET));
-        StringBuilder string = new StringBuilder();
-        char[] chars = new char[BUFFER_SIZE];
-        int bytes;
-        while ((bytes = reader.read(chars)) != -1) {
-            string.append(chars, 0, bytes);
-        }
-        return string.toString();
-    }
-
-    public void setConnectTimeout(int timeoutMillis) {
-        mConnectTimeout = timeoutMillis;
-    }
-
-    public void setReadTimeout(int timeoutMillis) {
-        mReadTimeout = timeoutMillis;
-    }
-
-    /**
-     * A Url rewriter that does nothing, i.e., returns the
-     * url that is passed to it.
-     */
-    public static class PassThroughRewriter implements UrlRewriter {
-        @Override
-        public String rewrite(String url) {
-            return url;
-        }
-    }
+  /**
+   * Creates a new HTTP helper.
+   *
+   * @param rewriter URI rewriter
+   * @param userAgent User agent string, e.g. "MyApp/1.0".
+   */
+  init {
+    mUserAgent = userAgent + " (" + Build.DEVICE + " " + Build.ID + ")"
+    mRewriter = rewriter
+  }
 }
