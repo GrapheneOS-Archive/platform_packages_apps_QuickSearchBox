09d99259b9600d116909a3a877e69ee79f538ec8
Ryan O'Leary <ryanaoleary@google.com>
2022-08-16 23:02:28 +0000

    Initial conversion of LevenshteinDistance.java to LevenshteinDistance.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I60efcf7a2a67919937994acf7ebbd8a966edce3f

--- src/com/android/quicksearchbox/util/LevenshteinDistance.kt
+++ src/com/android/quicksearchbox/util/LevenshteinDistance.kt
@@ -14,181 +14,157 @@
  * limitations under the License.
  */
 
-package com.android.quicksearchbox.util;
+package com.android.quicksearchbox.util
 
 /**
- * This class represents the matrix used in the Levenshtein distance algorithm, together
- * with the algorithm itself which operates on the matrix.
+ * This class represents the matrix used in the Levenshtein distance algorithm, together with the
+ * algorithm itself which operates on the matrix.
  *
- * We also track of the individual operations applied to transform the source string into the
- * target string so we can trace the path taken through the matrix afterwards, in order to
- * perform the formatting as required.
+ * We also track of the individual operations applied to transform the source string into the target
+ * string so we can trace the path taken through the matrix afterwards, in order to perform the
+ * formatting as required.
  */
-public class LevenshteinDistance {
-    public static final int EDIT_DELETE = 0;
-    public static final int EDIT_INSERT = 1;
-    public static final int EDIT_REPLACE = 2;
-    public static final int EDIT_UNCHANGED = 3;
+class LevenshteinDistance(
+  source: Array<LevenshteinDistance.Token>,
+  target: Array<LevenshteinDistance.Token>
+) {
+  private val mSource: Array<LevenshteinDistance.Token>
+  private val mTarget: Array<LevenshteinDistance.Token>
+  private val mEditTypeTable: Array<IntArray>
+  private val mDistanceTable: Array<IntArray>
 
-    private final Token[] mSource;
-    private final Token[] mTarget;
-    private final int[][] mEditTypeTable;
-    private final int[][] mDistanceTable;
+  /**
+   * Implementation of Levenshtein distance algorithm.
+   *
+   * @return The Levenshtein distance.
+   */
+  fun calculate(): Int {
+    val src = mSource
+    val trg = mTarget
+    val sourceLen = src.size
+    val targetLen = trg.size
+    val distTab = mDistanceTable
+    val editTab = mEditTypeTable
+    for (s in 1..sourceLen) {
+      val sourceToken = src[s - 1]
+      for (t in 1..targetLen) {
+        val targetToken = trg[t - 1]
+        val cost = if (sourceToken.prefixOf(targetToken)) 0 else 1
+        var distance = distTab[s - 1][t] + 1
+        var type: Int = LevenshteinDistance.Companion.EDIT_DELETE
+        var d = distTab[s][t - 1]
+        if (d + 1 < distance) {
+          distance = d + 1
+          type = LevenshteinDistance.Companion.EDIT_INSERT
+        }
+        d = distTab[s - 1][t - 1]
+        if (d + cost < distance) {
+          distance = d + cost
+          type =
+            if (cost == 0) LevenshteinDistance.Companion.EDIT_UNCHANGED
+            else LevenshteinDistance.Companion.EDIT_REPLACE
+        }
+        distTab[s][t] = distance
+        editTab[s][t] = type
+      }
+    }
+    return distTab[sourceLen][targetLen]
+  }
 
-    public LevenshteinDistance(Token[] source, Token[] target) {
-        final int sourceSize = source.length;
-        final int targetSize = target.length;
-        final int[][] editTab = new int[sourceSize+1][targetSize+1];
-        final int[][] distTab = new int[sourceSize+1][targetSize+1];
-        editTab[0][0] = EDIT_UNCHANGED;
-        distTab[0][0] = 0;
-        for (int i = 1; i <= sourceSize; ++i) {
-            editTab[i][0] = EDIT_DELETE;
-            distTab[i][0] = i;
+  /**
+   * Gets the list of operations which were applied to each target token; [.calculate] must have
+   * been called on this object before using this method.
+   * @return A list of [EditOperation]s indicating the origin of each token in the target string.
+   * The position of the token indicates the position in the source string of the token that was
+   * unchanged/replaced, or the position in the source after which a target token was inserted.
+   */
+  val targetOperations: Array<EditOperation?>
+    get() {
+      val trgLen = mTarget.size
+      val ops = arrayOfNulls<EditOperation>(trgLen)
+      var targetPos = trgLen
+      var sourcePos = mSource.size
+      val editTab = mEditTypeTable
+      while (targetPos > 0) {
+        val editType = editTab[sourcePos][targetPos]
+        when (editType) {
+          LevenshteinDistance.Companion.EDIT_DELETE -> sourcePos--
+          LevenshteinDistance.Companion.EDIT_INSERT -> {
+            targetPos--
+            ops[targetPos] = EditOperation(editType, sourcePos)
+          }
+          LevenshteinDistance.Companion.EDIT_UNCHANGED,
+          LevenshteinDistance.Companion.EDIT_REPLACE -> {
+            targetPos--
+            sourcePos--
+            ops[targetPos] = EditOperation(editType, sourcePos)
+          }
         }
-        for (int i = 1; i <= targetSize; ++i) {
-            editTab[0][i] = EDIT_INSERT;
-            distTab[0][i] = i;
-        }
-        mEditTypeTable = editTab;
-        mDistanceTable = distTab;
-        mSource = source;
-        mTarget = target;
+      }
+      return ops
     }
 
-    /**
-     * Implementation of Levenshtein distance algorithm.
-     *
-     * @return The Levenshtein distance.
-     */
-    public int calculate() {
-        final Token[] src = mSource;
-        final Token[] trg = mTarget;
-        final int sourceLen = src.length;
-        final int targetLen = trg.length;
-        final int[][] distTab = mDistanceTable;
-        final int[][] editTab = mEditTypeTable;
-        for (int s = 1; s <= sourceLen; ++s) {
-            Token sourceToken = src[s-1];
-            for (int t = 1; t <= targetLen; ++t) {
-                Token targetToken = trg[t-1];
-                int cost = sourceToken.prefixOf(targetToken) ? 0 : 1;
-
-                int distance = distTab[s-1][t] + 1;
-                int type = EDIT_DELETE;
-
-                int d = distTab[s][t - 1];
-                if (d + 1 < distance ) {
-                    distance = d + 1;
-                    type = EDIT_INSERT;
-                }
-
-                d = distTab[s - 1][t - 1];
-                if (d + cost < distance) {
-                    distance = d + cost;
-                    type = cost == 0 ? EDIT_UNCHANGED : EDIT_REPLACE;
-                }
-                distTab[s][t] = distance;
-                editTab[s][t] = type;
-            }
-        }
-        return distTab[sourceLen][targetLen];
+  class EditOperation(val type: Int, val position: Int)
+  class Token(private val mContainer: CharArray, val mStart: Int, val mEnd: Int) : CharSequence {
+    fun length(): Int {
+      return mEnd - mStart
     }
 
-    /**
-     * Gets the list of operations which were applied to each target token; {@link #calculate} must
-     * have been called on this object before using this method.
-     * @return A list of {@link EditOperation}s indicating the origin of each token in the target
-     *      string. The position of the token indicates the position in the source string of the
-     *      token that was unchanged/replaced, or the position in the source after which a target
-     *      token was inserted.
-     */
-    public EditOperation[] getTargetOperations() {
-        final int trgLen = mTarget.length;
-        final EditOperation[] ops = new EditOperation[trgLen];
-        int targetPos = trgLen;
-        int sourcePos = mSource.length;
-        final int[][] editTab = mEditTypeTable;
-        while (targetPos > 0) {
-            int editType = editTab[sourcePos][targetPos];
-            switch (editType) {
-                case LevenshteinDistance.EDIT_DELETE:
-                    sourcePos--;
-                    break;
-                case LevenshteinDistance.EDIT_INSERT:
-                    targetPos--;
-                    ops[targetPos] = new EditOperation(editType, sourcePos);
-                    break;
-                case LevenshteinDistance.EDIT_UNCHANGED:
-                case LevenshteinDistance.EDIT_REPLACE:
-                    targetPos--;
-                    sourcePos--;
-                    ops[targetPos] = new EditOperation(editType, sourcePos);
-                    break;
-            }
-        }
-
-        return ops;
+    @Override
+    override fun toString(): String {
+      // used in tests only.
+      return subSequence(0, length())
     }
 
-    public static final class EditOperation {
-        private final int mType;
-        private final int mPosition;
-        public EditOperation(int type, int position) {
-            mType = type;
-            mPosition = position;
+    fun prefixOf(that: LevenshteinDistance.Token): Boolean {
+      val len = length()
+      if (len > that.length()) return false
+      val thisStart = mStart
+      val thatStart: Int = that.mStart
+      val thisContainer = mContainer
+      val thatContainer: CharArray = that.mContainer
+      for (i in 0 until len) {
+        if (thisContainer[thisStart + i] != thatContainer[thatStart + i]) {
+          return false
         }
-        public int getType() {
-            return mType;
-        }
-        public int getPosition() {
-            return mPosition;
-        }
+      }
+      return true
     }
 
-    public static final class Token implements CharSequence {
-        private final char[] mContainer;
-        public final int mStart;
-        public final int mEnd;
-
-        public Token(char[] container, int start, int end) {
-            mContainer = container;
-            mStart = start;
-            mEnd = end;
-        }
-
-        public int length() {
-            return mEnd - mStart;
-        }
-
-        @Override
-        public String toString() {
-            // used in tests only.
-            return subSequence(0, length());
-        }
-
-        public boolean prefixOf(final Token that) {
-            final int len = length();
-            if (len > that.length()) return false;
-            final int thisStart = mStart;
-            final int thatStart = that.mStart;
-            final char[] thisContainer = mContainer;
-            final char[] thatContainer = that.mContainer;
-            for (int i = 0; i < len; ++i) {
-                if (thisContainer[thisStart + i] != thatContainer[thatStart + i]) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public char charAt(int index) {
-            return mContainer[index + mStart];
-        }
-
-        public String subSequence(int start, int end) {
-            return new String(mContainer, mStart + start, length());
-        }
-
+    fun charAt(index: Int): Char {
+      return mContainer[index + mStart]
     }
+
+    override fun subSequence(start: Int, end: Int): String {
+      return String(mContainer, mStart + start, length())
+    }
+  }
+
+  companion object {
+    const val EDIT_DELETE = 0
+    const val EDIT_INSERT = 1
+    const val EDIT_REPLACE = 2
+    const val EDIT_UNCHANGED = 3
+  }
+
+  init {
+    val sourceSize = source.size
+    val targetSize = target.size
+    val editTab = Array(sourceSize + 1) { IntArray(targetSize + 1) }
+    val distTab = Array(sourceSize + 1) { IntArray(targetSize + 1) }
+    editTab[0][0] = LevenshteinDistance.Companion.EDIT_UNCHANGED
+    distTab[0][0] = 0
+    for (i in 1..sourceSize) {
+      editTab[i][0] = LevenshteinDistance.Companion.EDIT_DELETE
+      distTab[i][0] = i
+    }
+    for (i in 1..targetSize) {
+      editTab[0][i] = LevenshteinDistance.Companion.EDIT_INSERT
+      distTab[0][i] = i
+    }
+    mEditTypeTable = editTab
+    mDistanceTable = distTab
+    mSource = source
+    mTarget = target
+  }
 }
