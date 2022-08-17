edce764a646911c6db8db06a8a8fb8990a378cf1
Ryan O'Leary <ryanaoleary@google.com>
2022-08-17 07:39:40 +0000

    Initial conversion of PriorityThreadFactory.java to PriorityThreadFactory.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: If4ba16aba19d9a79a0e6e38d5c400cc5f7c01fe7

--- src/com/android/quicksearchbox/util/PriorityThreadFactory.kt
+++ src/com/android/quicksearchbox/util/PriorityThreadFactory.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,38 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.quicksearchbox.util
 
-package com.android.quicksearchbox.util;
-
-import android.os.Process;
-
-import java.util.concurrent.ThreadFactory;
+import android.os.Process
+import java.util.concurrent.ThreadFactory
 
 /**
  * A thread factory that creates threads with a given thread priority.
  */
-public class PriorityThreadFactory implements ThreadFactory {
-
-    private final int mPriority;
-
-    /**
-     * Creates a new thread factory.
-     *
-     * @param priority The thread priority of the threads created by this factory.
-     *        For values, see {@link Process}.
-     */
-    public PriorityThreadFactory(int priority) {
-        mPriority = priority;
-    }
-
-    public Thread newThread(Runnable r) {
-        return new Thread(r) {
+class PriorityThreadFactory
+/**
+ * Creates a new thread factory.
+ *
+ * @param priority The thread priority of the threads created by this factory.
+ * For values, see [Process].
+ */(private val mPriority: Int) : ThreadFactory {
+    fun newThread(r: Runnable?): Thread {
+        return object : Thread(r) {
             @Override
-            public void run() {
-                Process.setThreadPriority(mPriority);
-                super.run();
+            fun run() {
+                Process.setThreadPriority(mPriority)
+                super.run()
             }
-        };
+        }
     }
-
 }
