40e944bff5bfd1f240f627991cf39aba71de2f5e
Ryan O'Leary <ryanaoleary@google.com>
2022-08-22 20:31:54 +0000

    Initial conversion of DefaultSuggestionView.java to DefaultSuggestionView.kt in AOSP QuickSearchBox App using automatic conversion tool.
    Test: built against Kotlin 1.7, did not compile successfully but still commiting to showcase git history of conversion
    Change-Id: I9372389cb5c8d53d048e921fd641527e140b0b84

--- src/com/android/quicksearchbox/ui/DefaultSuggestionView.kt
+++ src/com/android/quicksearchbox/ui/DefaultSuggestionView.kt
@@ -14,241 +14,227 @@
  * limitations under the License.
  */
 
-package com.android.quicksearchbox.ui;
+package com.android.quicksearchbox.ui
 
-import com.android.quicksearchbox.R;
-import com.android.quicksearchbox.Source;
-import com.android.quicksearchbox.Suggestion;
-import com.android.quicksearchbox.util.Consumer;
-import com.android.quicksearchbox.util.NowOrLater;
-
-import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.TextUtils;
-import android.text.style.TextAppearanceSpan;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import com.android.quicksearchbox.R
+import com.android.quicksearchbox.Source
+import com.android.quicksearchbox.Suggestion
+import com.android.quicksearchbox.util.Consumer
+import com.android.quicksearchbox.util.NowOrLater
 
 /**
  * View for the items in the suggestions list. This includes promoted suggestions,
  * sources, and suggestions under each source.
  */
-public class DefaultSuggestionView extends BaseSuggestionView {
+class DefaultSuggestionView : BaseSuggestionView {
+    private val TAG = "QSB.DefaultSuggestionView"
+    private var mAsyncIcon1: DefaultSuggestionView.AsyncIcon? = null
+    private var mAsyncIcon2: DefaultSuggestionView.AsyncIcon? = null
 
-    private static final boolean DBG = false;
-
-    private static final String VIEW_ID = "default";
-
-    private final String TAG = "QSB.DefaultSuggestionView";
-
-    private AsyncIcon mAsyncIcon1;
-    private AsyncIcon mAsyncIcon2;
-
-    public DefaultSuggestionView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
     }
 
-    public DefaultSuggestionView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public DefaultSuggestionView(Context context) {
-        super(context);
-    }
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?) : super(context) {}
 
     @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        mText1 = (TextView) findViewById(R.id.text1);
-        mText2 = (TextView) findViewById(R.id.text2);
-        mAsyncIcon1 = new AsyncIcon(mIcon1) {
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mText1 = findViewById(R.id.text1) as TextView
+        mText2 = findViewById(R.id.text2) as TextView
+        mAsyncIcon1 = object : DefaultSuggestionView.AsyncIcon(mIcon1) {
             // override default icon (when no other available) with default source icon
             @Override
-            protected String getFallbackIconId(Source source) {
-                return source.getSourceIconUri().toString();
+            protected override fun getFallbackIconId(source: Source): String {
+                return source.getSourceIconUri().toString()
             }
+
             @Override
-            protected Drawable getFallbackIcon(Source source) {
-                return source.getSourceIcon();
+            protected override fun getFallbackIcon(source: Source): Drawable {
+                return source.getSourceIcon()
             }
-        };
-        mAsyncIcon2 = new AsyncIcon(mIcon2);
+        }
+        mAsyncIcon2 = DefaultSuggestionView.AsyncIcon(mIcon2)
     }
 
     @Override
-    public void bindAsSuggestion(Suggestion suggestion, String userQuery) {
-        super.bindAsSuggestion(suggestion, userQuery);
-
-        CharSequence text1 = formatText(suggestion.getSuggestionText1(), suggestion);
-        CharSequence text2 = suggestion.getSuggestionText2Url();
-        if (text2 != null) {
-            text2 = formatUrl(text2);
-        } else {
-            text2 = formatText(suggestion.getSuggestionText2(), suggestion);
-        }
+    override fun bindAsSuggestion(suggestion: Suggestion, userQuery: String) {
+        super.bindAsSuggestion(suggestion, userQuery)
+        val text1 = formatText(suggestion.getSuggestionText1(), suggestion)
+        var text2: CharSequence = suggestion.getSuggestionText2Url()
+        text2 =
+            text2?.let { formatUrl(it) } ?: formatText(suggestion.getSuggestionText2(), suggestion)
         // If there is no text for the second line, allow the first line to be up to two lines
         if (TextUtils.isEmpty(text2)) {
-            mText1.setSingleLine(false);
-            mText1.setMaxLines(2);
-            mText1.setEllipsize(TextUtils.TruncateAt.START);
+            mText1.setSingleLine(false)
+            mText1.setMaxLines(2)
+            mText1.setEllipsize(TextUtils.TruncateAt.START)
         } else {
-            mText1.setSingleLine(true);
-            mText1.setMaxLines(1);
-            mText1.setEllipsize(TextUtils.TruncateAt.MIDDLE);
+            mText1.setSingleLine(true)
+            mText1.setMaxLines(1)
+            mText1.setEllipsize(TextUtils.TruncateAt.MIDDLE)
         }
-        setText1(text1);
-        setText2(text2);
-        mAsyncIcon1.set(suggestion.getSuggestionSource(), suggestion.getSuggestionIcon1());
-        mAsyncIcon2.set(suggestion.getSuggestionSource(), suggestion.getSuggestionIcon2());
-
-        if (DBG) {
-            Log.d(TAG, "bindAsSuggestion(), text1=" + text1 + ",text2=" + text2 + ",q='" +
-                    userQuery + ",fromHistory=" + isFromHistory(suggestion));
+        setText1(text1)
+        setText2(text2)
+        mAsyncIcon1.set(suggestion.getSuggestionSource(), suggestion.getSuggestionIcon1())
+        mAsyncIcon2.set(suggestion.getSuggestionSource(), suggestion.getSuggestionIcon2())
+        if (DefaultSuggestionView.Companion.DBG) {
+            Log.d(
+                TAG, "bindAsSuggestion(), text1=" + text1 + ",text2=" + text2 + ",q='" +
+                        userQuery + ",fromHistory=" + isFromHistory(suggestion)
+            )
         }
     }
 
-    private CharSequence formatUrl(CharSequence url) {
-        SpannableString text = new SpannableString(url);
-        ColorStateList colors = getResources().getColorStateList(R.color.url_text);
-        text.setSpan(new TextAppearanceSpan(null, 0, 0, colors, null),
-                0, url.length(),
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return text;
+    private fun formatUrl(url: CharSequence): CharSequence {
+        val text = SpannableString(url)
+        val colors: ColorStateList = getResources().getColorStateList(R.color.url_text)
+        text.setSpan(
+            TextAppearanceSpan(null, 0, 0, colors, null),
+            0, url.length(),
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        return text
     }
 
-    private CharSequence formatText(String str, Suggestion suggestion) {
-        boolean isHtml = "html".equals(suggestion.getSuggestionFormat());
-        if (isHtml && looksLikeHtml(str)) {
-            return Html.fromHtml(str);
+    private fun formatText(str: String, suggestion: Suggestion): CharSequence {
+        val isHtml = "html".equals(suggestion.getSuggestionFormat())
+        return if (isHtml && looksLikeHtml(str)) {
+            Html.fromHtml(str)
         } else {
-            return str;
+            str
         }
     }
 
-    private boolean looksLikeHtml(String str) {
-        if (TextUtils.isEmpty(str)) return false;
-        for (int i = str.length() - 1; i >= 0; i--) {
-            char c = str.charAt(i);
-            if (c == '>' || c == '&') return true;
+    private fun looksLikeHtml(str: String): Boolean {
+        if (TextUtils.isEmpty(str)) return false
+        for (i in str.length() - 1 downTo 0) {
+            val c: Char = str.charAt(i)
+            if (c == '>' || c == '&') return true
         }
-        return false;
+        return false
     }
 
-    /**
-     * Sets the drawable in an image view, makes sure the view is only visible if there
-     * is a drawable.
-     */
-    private static void setViewDrawable(ImageView v, Drawable drawable) {
-        // Set the icon even if the drawable is null, since we need to clear any
-        // previous icon.
-        v.setImageDrawable(drawable);
-
-        if (drawable == null) {
-            v.setVisibility(View.GONE);
-        } else {
-            v.setVisibility(View.VISIBLE);
-
-            // This is a hack to get any animated drawables (like a 'working' spinner)
-            // to animate. You have to setVisible true on an AnimationDrawable to get
-            // it to start animating, but it must first have been false or else the
-            // call to setVisible will be ineffective. We need to clear up the story
-            // about animated drawables in the future, see http://b/1878430.
-            drawable.setVisible(false, false);
-            drawable.setVisible(true, false);
-        }
-    }
-
-    private class AsyncIcon {
-        private final ImageView mView;
-        private String mCurrentId;
-        private String mWantedId;
-
-        public AsyncIcon(ImageView view) {
-            mView = view;
-        }
-
-        public void set(final Source source, final String sourceIconId) {
+    private open inner class AsyncIcon(view: ImageView) {
+        private val mView: ImageView
+        private var mCurrentId: String? = null
+        private var mWantedId: String? = null
+        operator fun set(source: Source, sourceIconId: String?) {
             if (sourceIconId != null) {
                 // The iconId can just be a package-relative resource ID, which may overlap with
                 // other packages. Make sure it's globally unique.
-                Uri iconUri = source.getIconUri(sourceIconId);
-                final String uniqueIconId = iconUri == null ? null : iconUri.toString();
-                mWantedId = uniqueIconId;
+                val iconUri: Uri? = source.getIconUri(sourceIconId)
+                val uniqueIconId: String? = if (iconUri == null) null else iconUri.toString()
+                mWantedId = uniqueIconId
                 if (!TextUtils.equals(mWantedId, mCurrentId)) {
-                    if (DBG) Log.d(TAG, "getting icon Id=" + uniqueIconId);
-                    NowOrLater<Drawable> icon = source.getIcon(sourceIconId);
-                    if (icon.haveNow()) {
-                        if (DBG) Log.d(TAG, "getIcon ready now");
-                        handleNewDrawable(icon.getNow(), uniqueIconId, source);
+                    if (DefaultSuggestionView.Companion.DBG) Log.d(
+                        TAG,
+                        "getting icon Id=$uniqueIconId"
+                    )
+                    val icon: NowOrLater<Drawable?>? = source.getIcon(sourceIconId)
+                    if (icon!!.haveNow()) {
+                        if (DefaultSuggestionView.Companion.DBG) Log.d(TAG, "getIcon ready now")
+                        handleNewDrawable(icon.getNow(), uniqueIconId, source)
                     } else {
                         // make sure old icon is not visible while new one is loaded
-                        if (DBG) Log.d(TAG , "getIcon getting later");
-                        clearDrawable();
-                        icon.getLater(new Consumer<Drawable>(){
+                        if (DefaultSuggestionView.Companion.DBG) Log.d(TAG, "getIcon getting later")
+                        clearDrawable()
+                        icon.getLater(object : Consumer<Drawable?> {
                             @Override
-                            public boolean consume(Drawable icon) {
-                                if (DBG) {
-                                    Log.d(TAG, "IconConsumer.consume got id " + uniqueIconId +
-                                            " want id " + mWantedId);
+                            override fun consume(icon: Drawable): Boolean {
+                                if (DefaultSuggestionView.Companion.DBG) {
+                                    Log.d(
+                                        TAG, "IconConsumer.consume got id " + uniqueIconId +
+                                                " want id " + mWantedId
+                                    )
                                 }
                                 // ensure we have not been re-bound since the request was made.
                                 if (TextUtils.equals(uniqueIconId, mWantedId)) {
-                                    handleNewDrawable(icon, uniqueIconId, source);
-                                    return true;
+                                    handleNewDrawable(icon, uniqueIconId, source)
+                                    return true
                                 }
-                                return false;
-                            }});
+                                return false
+                            }
+                        })
                     }
                 }
             } else {
-                mWantedId = null;
-                handleNewDrawable(null, null, source);
+                mWantedId = null
+                handleNewDrawable(null, null, source)
             }
         }
 
-        private void handleNewDrawable(Drawable icon, String id, Source source) {
+        private fun handleNewDrawable(icon: Drawable?, id: String?, source: Source) {
+            var icon: Drawable? = icon
             if (icon == null) {
-                mWantedId = getFallbackIconId(source);
+                mWantedId = getFallbackIconId(source)
                 if (TextUtils.equals(mWantedId, mCurrentId)) {
-                    return;
+                    return
                 }
-                icon = getFallbackIcon(source);
+                icon = getFallbackIcon(source)
             }
-            setDrawable(icon, id);
+            setDrawable(icon, id)
         }
 
-        private void setDrawable(Drawable icon, String id) {
-            mCurrentId = id;
-            setViewDrawable(mView, icon);
+        private fun setDrawable(icon: Drawable?, id: String?) {
+            mCurrentId = id
+            DefaultSuggestionView.Companion.setViewDrawable(mView, icon)
         }
 
-        private void clearDrawable() {
-            mCurrentId = null;
-            mView.setImageDrawable(null);
+        private fun clearDrawable() {
+            mCurrentId = null
+            mView.setImageDrawable(null)
         }
 
-        protected String getFallbackIconId(Source source) {
-            return null;
+        protected open fun getFallbackIconId(source: Source?): String? {
+            return null
         }
 
-        protected Drawable getFallbackIcon(Source source) {
-            return null;
+        protected open fun getFallbackIcon(source: Source?): Drawable? {
+            return null
         }
 
-    }
-
-    public static class Factory extends SuggestionViewInflater {
-        public Factory(Context context) {
-            super(VIEW_ID, DefaultSuggestionView.class, R.layout.suggestion, context);
+        init {
+            mView = view
         }
     }
 
+    class Factory(context: Context?) : SuggestionViewInflater(
+        DefaultSuggestionView.Companion.VIEW_ID,
+        DefaultSuggestionView::class.java,
+        R.layout.suggestion,
+        context
+    )
+
+    companion object {
+        private const val DBG = false
+        private const val VIEW_ID = "default"
+
+        /**
+         * Sets the drawable in an image view, makes sure the view is only visible if there
+         * is a drawable.
+         */
+        private fun setViewDrawable(v: ImageView, drawable: Drawable?) {
+            // Set the icon even if the drawable is null, since we need to clear any
+            // previous icon.
+            v.setImageDrawable(drawable)
+            if (drawable == null) {
+                v.setVisibility(View.GONE)
+            } else {
+                v.setVisibility(View.VISIBLE)
+
+                // This is a hack to get any animated drawables (like a 'working' spinner)
+                // to animate. You have to setVisible true on an AnimationDrawable to get
+                // it to start animating, but it must first have been false or else the
+                // call to setVisible will be ineffective. We need to clear up the story
+                // about animated drawables in the future, see http://b/1878430.
+                drawable.setVisible(false, false)
+                drawable.setVisible(true, false)
+            }
+        }
+    }
 }
